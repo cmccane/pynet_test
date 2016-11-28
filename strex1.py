e4fa5b951235d5b67889317dda56c8f4e9fb0587
Charles <cmccane@verizon.net>
2016-11-28 09:29:22 -0800

    dded strex1

---
+++ strex1.py
@@ -0,0 +1,12 @@
+name1 = "Robert Smith"
+name2 = "Kelly Rock"
+name3 = "Jimmy Dean"
+name4 = raw_input("Enter forth name: ")
+
+print
+print "{:>30}".format(name1)
+print "{:>30}".format(name2)
+print "{:>30}".format(name3)
+print "{:>30}".format(name4)
+print
+
